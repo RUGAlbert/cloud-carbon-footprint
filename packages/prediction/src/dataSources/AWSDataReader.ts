41dba113471717e753473b66983b75ec0378d374
albert <albertdijkstra1998@hotmail.com>
2022-06-15 13:01:58 +0200

    removed complexity

--- packages/prediction/src/dataSources/AWSDataReader.ts
+++ packages/prediction/src/dataSources/AWSDataReader.ts
@@ -5,16 +5,9 @@ import csv from 'csvtojson'
 import path from 'path'
 
 import { App } from '@cloud-carbon-footprint/app'
-export async function readAWSData(
-  AWSInputFile: string,
-): Promise<any[]> {
-
-  const AWSInputData = await csv().fromFile(AWSInputFile)
-  return AWSInputData;
-}
 
 export default async function getAWSData(
   ): Promise<any[]> {
   
-	return readAWSData("C:\\Users\\alber\\repositories\\school\\cloud-carbon-footprint\\packages\\prediction\\input\\instancetypes.csv");
+	return csv().fromFile("C:\\Users\\alber\\repositories\\school\\cloud-carbon-footprint\\packages\\prediction\\input\\instancetypes.csv");
   }
